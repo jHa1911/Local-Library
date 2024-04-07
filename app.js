9d7f2097b02e574406f819fae83be19b34f93cf7
jHa1911 <sjha13390@gmail.com>
2024-04-08 01:12:13 +0530

    Made Production ready by adding some modules

--- app.js
+++ app.js
@@ -3,17 +3,41 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalog = require("./routes/catalog"); // Import routes for "catalog" area of site
+const compression = require("compression");
+const helmet = require("helmet");
+
+
 
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://surajdb:WVaX03fy1hjiGk1p@cluster0.qmfbepu.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+const dev_db_url = "mongodb+srv://surajdb:WVaX03fy1hjiGk1p@cluster0.qmfbepu.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -31,6 +55,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(compression()); // Compress all routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/catalog", catalog); // Add catalog routes to middleware chain.
